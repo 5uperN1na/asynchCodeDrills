c1e7a2caed8fbe3b31f5d24d9ae4f80d723b7b41
Paula Suarez <Paula.Suarez@freeman.com>
2019-02-26 14:52:27 -0600

    Adding more files to repo.

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-//Timeout part 1
+//Timing Out (#1 through #3)
 
 //function to log a message to the console
 
@@ -17,7 +17,7 @@ setTimeout(() => {
 
 */
 
-//Timeout part 2
+//Timing Out (#4)
 /*
 function getWords(word) {
     console.log("Hola--1st");
@@ -37,32 +37,69 @@ function getWords(word) {
 getWords();
 */
 
+//Callbacks and Recursion
+
+//NOT COMPLETE:  still need to get the callback working for the done() function.
+
 function done() {
     console.log("The job is done!");
 
 }
 
-
 function countdown(num, callback) {
-    console.log(callback);
-
 
     setTimeout(() => {
         console.log(num);
 
         // TERMINATION
-        if (num < 0) return;
-        // BASE
-        if (num === 0) return 1;
+        if (num < 2) return;
+
         // RECURSION
-        return num * countdown(num - 1);
-        if (num === 1) return callback();
-          
-        
+        return countdown(num - 1);
+
+        // BASE
+        if (num === 0) return;
+
 
     }, 1000);
 
+    if (num === 1) {
+        console.log(callback);
+    }
 
 }
- 
-countdown(10, done);
+
+countdown(3, done);
+
+//Promises, Promises
+
+//global variable
+//if set to true, log food but if set to false, log error message
+let lunchtime = false;
+
+//create a promise that returns a new promise
+let orderMeSomeFood = new Promise(function (resolve, reject) {
+    if (lunchtime === true) {
+        let food = { lunch: "soup", drink: "iced tea" };
+        resolve(food);
+    } else {
+        let err = new Error("There is no more food!");
+        reject(err);
+    }
+});
+
+//function creation
+function orderLunch() {
+    orderMeSomeFood
+        .then(food => {
+            console.log(food);
+        })
+        .catch(err => {
+            console.log(err);
+        });
+}
+
+//invoke function
+orderLunch();
+
+
